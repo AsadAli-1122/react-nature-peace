eea0830f394d12e860ca04aad4f780dba5d8b5d6
AsadAli-1122 <asadali.cool.23@gmail.com>
2023-07-02 14:15:17 +0500

    update 1.1.1.0

---
+++ src/assets/pages/PlantDrop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlantDrop = () => {
+  return (
+    <div className='min-h-screen max-w-7xl mx-auto bg-white'>
+      {/* <p>PlantDrop</p> */}
+    </div>
+  )
+}
+
+export default PlantDrop;
